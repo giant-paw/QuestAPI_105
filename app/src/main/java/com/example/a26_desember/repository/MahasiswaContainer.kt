88dac2bf03a20c9c3c06f7e65eb1c7fa304347f3
giant-paw <giantpawbusiness@gmail.com>
2024-12-29 12:32:27 +0700

    memperbaiki agar muncul data di home dan tersambung dengan ip yang sedang berjalan

--- app/src/main/java/com/example/a26_desember/repository/MahasiswaContainer.kt
+++ app/src/main/java/com/example/a26_desember/repository/MahasiswaContainer.kt
@@ -11,7 +11,7 @@ interface AppContainer {
 }
 
 class MahasiswaContainer: AppContainer{
-    private val baseUrl = "http://10.0.2.2:8806/umyTI/"
+    private val baseUrl = "http://192.168.37.232/umyTI/"
 
     private val json = Json { ignoreUnknownKeys = true }
 
